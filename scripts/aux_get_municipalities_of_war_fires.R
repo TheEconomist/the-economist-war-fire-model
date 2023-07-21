75894946c2620879ea12f75d4247b9f7bcc1416e
Sondre Ulvund Solstad <sondresolstad@economist.com>
2023-07-21 20:19:20 +0100

    Update aux_get_municipalities_of_war_fires.R

--- scripts/aux_get_municipalities_of_war_fires.R
+++ scripts/aux_get_municipalities_of_war_fires.R
@@ -5,7 +5,7 @@ library(readr)
 library(tidyverse)
 
 # Load fires data:
-input <- read_csv('output-data/all_fires_all_cols_exact_pop_and_filter_2022_2023.csv')
+input <- read_csv('output-data/ukraine_war_fires.csv')
 
 # Load map of Ukrainian municipalities:
 # https://data.humdata.org/dataset/cod-ab-ukr?
@@ -26,27 +26,50 @@ check_points <- st_as_sf(check_points, coords = c('x', 'y'), crs = st_crs(ukrain
 check_points <- st_transform(check_points, crs = "EPSG:6381")
 ukraine <- st_transform(ukraine, crs = "EPSG:6381")
 
+# Load cache:
+known_points <- readRDS('output-data/model-objects/points_to_municipalities.RDS')
+
 # Loop through points and get relevant municipality:
-check_points$municipality <- NA
-for(i in 1:nrow(check_points)){
-  res <- check_points[i, ] %>% mutate(
-    intersection = as.integer(st_intersects(geometry, ukraine))
-    , area = if_else(is.na(intersection), '', ukraine$ADM3_PCODE[intersection])
-  )
-  check_points$municipality[i] <- res$area[1]
-  cat(paste0('\r\r ', round(i/nrow(check_points)*100, 2), '% complete ......... \r\r'))
+check_points <- check_points[!check_points$geometry %in% known_points$geometry, ]
+
+if(nrow(check_points) > 0){
+  check_points$municipality <- NA
+  for(i in 1:nrow(check_points)){
+    res <- check_points[i, ] %>% mutate(
+      intersection = as.integer(st_intersects(geometry, ukraine))
+      , area = if_else(is.na(intersection), '', ukraine$ADM3_PCODE[intersection])
+    )
+    check_points$municipality[i] <- res$area[1]
+    cat(paste0('\r\r ', round(i/nrow(check_points)*100, 2), '% complete ......... \r\r'))
+  }
+
+  known_points <- rbind(known_points, known_points)
 }
 
-# Merge back in with specific points:
-munis <- cbind.data.frame(check_points, unique(unique_locations[, c('x', 'y')]))
-munis <- merge(unique_locations, munis, by = c('x', 'y'), all = T)
+# Save cache
+saveRDS(known_points, 'output-data/model-objects/points_to_municipalities.RDS')
+
+# Merge back in with specific points
+known_points <- st_transform(known_points, "WGS84")
+known_points <- cbind.data.frame(known_points, st_coordinates(known_points))
+known_points$geometry <- NULL
+known_points$X <- round(known_points$X, 3) # R being R...
+known_points$Y <- round(known_points$Y, 3) # R being R...
+locations <- merge(data.frame(unique_locations),
+                   known_points[!is.na(known_points$municipality), ],
+                   by.x=c('x', 'y'),
+                   by.y=c('X', 'Y'), all.x = T)
 
 # Add in covariates to these municipalities:
 munis_covars <- as.data.frame(ukraine[, c(3:6, 14:22)])
-munis$ADM3_PCODE <- munis$municipality
-munis <- merge(munis, munis_covars, by = 'ADM3_PCODE', all.x = T)
-munis$geometry.x <- munis$geometry.y <- NULL
+locations$ADM3_PCODE <- locations$municipality
+locations <- merge(locations, munis_covars, by = 'ADM3_PCODE', all.x = T)
+locations$geometry <- NULL
+locations <- merge(locations, input[, c('LATITUDE', 'LONGITUDE', 'year', 'ACQ_TIME', 'date', 'pop_exact')], by=c('LATITUDE', 'LONGITUDE'), all.y=T)
+locations$x <- NULL
+locations$y <- NULL
 
 # Export:
-saveRDS(munis[, ], 'output-data/points_to_municipality_with_extra_cols.RDS')
-saveRDS(munis[, c('LATITUDE', 'LONGITUDE', 'municipality')], 'output-data/points_to_municipality.RDS')
+saveRDS(locations, 'output-data/points_to_municipality_with_extra_cols.RDS')
+saveRDS(locations[, c('LATITUDE', 'LONGITUDE', 'municipality')], 'output-data/points_to_municipality.RDS')
+write_csv(locations, 'output-data/war_fires_by_ADM3.csv')
