76877c727d01be71d1d2b42fb454081413e49bad
Sondre Ulvund Solstad <sondresolstad@economist.com>
2024-08-19 14:57:05 +0100

    added fires in near-by Russia since full-scale invasion of Ukraine began

--- scripts/aux_update_firms_data_RU.R
+++ scripts/aux_update_firms_data_RU.R
@@ -1,5 +1,33 @@
-# This script downloads the latest data from FIRMS and then saves it to the file "output-data/fires_update.csv"
+# This script downloads the latest data from FIRMS and then saves it to the file "output-data/fires_update_RU.csv"
 library(readr)
+
+archive_cache <- T
+if(!archive_cache){
+  # Get archive:
+  library(sf)
+  j1v <- st_read('/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/DL_FIRE_J1V-C2_501987/fire_nrt_J1V-C2_501987.shp')
+  j2v <- st_read('/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/DL_FIRE_J2V-C2_501988/fire_nrt_J2V-C2_501988.shp')
+  m <- st_read('/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/DL_FIRE_M-C61_501986/fire_archive_M-C61_501986.shp')
+  sv <- st_read('/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/DL_FIRE_SV-C2_501989/fire_archive_SV-C2_501989.shp')
+
+  # Combine the data
+  archive <- do.call(rbind, lapply(list(j1v, j2v, m, sv), function(df) df[, Reduce(intersect, list(colnames(j1v), colnames(j2v), colnames(m), colnames(sv))), drop = FALSE]))
+
+  colnames(archive) <- tolower(colnames(archive))
+  archive$country_id <- 'RUS'
+  archive <- archive[, c("country_id", "latitude", "longitude", "scan", "track",
+                         "acq_date", "acq_time", "satellite", "instrument",
+                         "confidence", "version", "frp", "daynight")]
+  archive <- archive[!duplicated(paste0(archive$latitude, '-', archive$longitude, '-', archive$acq_date, '-', archive$acq_time, '-', archive$satellite)), ]
+  write_csv(archive, '/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/archive_big.csv')
+  write_csv(archive[as.Date(archive$acq_date) >= as.Date('2020-01-01'), ], '/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/archive_since_2020.csv')
+  write_csv(archive[as.Date(archive$acq_date) >= as.Date('2022-01-01'), ], '/Users/sondresolstad/Github/war-torch-detection-system/source-data/fire-archive-data/RUS/archive_since_2022.csv')
+
+  old_fires <- read_csv('output-data/firms_update_RU.csv')
+  old_fires <- rbind(old_fires, archive[as.Date(archive$acq_date) >= as.Date('2022-02-21'), colnames(old_fires)])
+  old_fires[!duplicated(paste0(old_fires$latitude, '-', old_fires$longitude, '-', old_fires$acq_date, '-', old_fires$acq_time, '-', old_fires$satellite)), ]
+  write_csv(old_fires, 'output-data/firms_update_RU.csv')
+}
 
 # Define map key and sat systems used
 map_key <- Sys.getenv("FIRMS_API_KEY")
