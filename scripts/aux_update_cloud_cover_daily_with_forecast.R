b7070e127c1d60a7b524d93444ace649fe652eb8
Sondre Ulvund Solstad <sondresolstad@economist.com>
2024-08-20 10:23:09 +0100

    fix to historical cloud coverage

--- scripts/aux_update_cloud_cover_daily_with_forecast.R
+++ scripts/aux_update_cloud_cover_daily_with_forecast.R
@@ -25,6 +25,30 @@ get_weather_forecast_by_lat_lng <- function(url){
   return(df)
 }
 
+if(update_historical){
+  historical <- data.frame()
+  start_date <- min(clouds$date[is.na(clouds$cloud_cover_in_country)], na.rm = T)
+  end_date <- Sys.Date()
+  for(i in 1:nrow(grid)[1]){
+    url <- paste0('https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=', grid$y[i], '&longitude=', grid$x[i], '&hourly=temperature_2m,cloudcover&start_date=', start_date, '&end_date=', end_date)
+    historical <- bind_rows(historical,get_weather_forecast_by_lat_lng(url))
+    print(summary(historical$hourly.cloudcover))
+    cat('.')
+  }
+  # Simplify
+  historical$date <- anydate(historical$hourly.time)
+  historical$hourly.cloudcover[historical$hourly.cloudcover < 0] <- 0
+  historical$hourly.cloudcover[historical$hourly.cloudcover > 100] <- 100
+  historical$hourly.cloudcover <- historical$hourly.cloudcover/100
+  historical$cloud_cover_in_country <- ave(historical$hourly.cloudcover, historical$date, FUN = function(x) mean(x, na.rm = T))
+  historical <- historical[historical$longitude >= 30, ]
+  historical$cloud_cover_in_east_of_country <- ave(historical$hourly.cloudcover, historical$date, FUN = function(x) mean(x, na.rm = T))
+  historical <- unique(historical[, c('date', 'cloud_cover_in_country', 'cloud_cover_in_east_of_country')])
+  clouds <- clouds[!clouds$date %in% historical$date, ]
+  clouds <- rbind(clouds, historical)
+  write_csv(clouds, 'output-data/cloud_cover_in_ukraine_by_day.csv')
+}
+
 # Loop through locations and save results:
 df <- data.frame()
 for(i in 1:nrow(grid)){
@@ -39,10 +63,10 @@ df$hourly.cloudcover <- df$hourly.cloudcover/100
 df$cloud_cover_in_country <- ave(df$hourly.cloudcover, df$date, FUN = function(x) mean(x, na.rm = T))
 df <- df[df$longitude >= 30, ]
 df$cloud_cover_in_east_of_country <- ave(df$hourly.cloudcover, df$date, FUN = function(x) mean(x, na.rm = T))
-df <- unique(df[, c('date', 'cloud_cover_in_country', 'cloud_cover_in_east_of_country')])
+df <- na.omit(unique(df[, c('date', 'cloud_cover_in_country', 'cloud_cover_in_east_of_country')]))
 
 # Save
-clouds <- clouds[clouds$date < start_date, ]
+clouds <- clouds[!clouds$date %in% df$date, ]
 clouds <- rbind(clouds, df)
 write_csv(clouds, 'output-data/cloud_cover_in_ukraine_by_day_with_forecast_for_recent_days.csv')
 
